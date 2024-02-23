48f3031179e320568d4e8603a4acd0b01adb1f9a
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-23 08:10:20 +0100

    cleanup

--- app/routes/use-debounce.tsx
+++ app/routes/use-debounce.tsx
@@ -1,6 +1,21 @@
 import type { ChangeEvent } from "react";
-import { useState } from "react";
-import { useDebounce } from "~/hooks/useDebounce";
+import { useEffect, useState } from "react";
+
+function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
 
 export default function UseDebounce() {
   const [searchTerm, setSearchTerm] = useState("");
